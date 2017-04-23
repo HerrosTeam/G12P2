4084915e21ecc9dd47fbfba55b5caffa2fc6b7f6
laura-h05 <laurahernandoserrano@gmail.com>
2017-04-23 22:50:55 +0200

    Working ERX

--- src/main/java/es/ucm/pev/g12p2/crossover/ERX.java
+++ src/main/java/es/ucm/pev/g12p2/crossover/ERX.java
@@ -8,12 +8,13 @@ package es.ucm.pev.g12p2.crossover;
 import es.ucm.pev.g12p2.chromosome.Chromosome;
 import es.ucm.pev.g12p2.chromosome.Function;
 import es.ucm.pev.g12p2.chromosome.gene.Gene;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -29,84 +30,66 @@ public class ERX extends Crossover {
         List<Chromosome> children = new LinkedList();
 
         Function c = (Function) parent1;
-        
         Chromosome child1 = new Function(0.05, parent1.getLength(), false, c.getData());
         Chromosome child2 = new Function(0.05, parent1.getLength(), false, c.getData());
-        
+
         int distance[][] = c.getDistanceData();
-        boolean majorConflict = true;
-        
-        int j=0;
-        do{
-            majorConflict = false;
-            child1 = new Function(0.05, parent1.getLength(), false, c.getData());
-            child1.getGene(0).addAllele(0, (int)parent2.getGene(j).getAllele(0));
-            int chosenCity=(int)parent2.getGene(j).getAllele(0);
-            for(int i=1; i<child1.getLength() && !majorConflict ;i++){
-                boolean conflict=false;
-                do{
-                    int aux = chosenCity;
-                    List<Integer> leastConnectedCities = this.getLeastConnectedCities(chosenCity, parent1.getLength(), distance);
-                    chosenCity = this.getChosenCity(leastConnectedCities);
-                    //determine if there is conflict
-                    conflict = false;
-                    for(Gene g: child1.getGenes()){
-                        if(g.getSize() != 0){
-                            if(g.getAllele(0).equals(chosenCity)){
-                                conflict = true;
-                                break;
-                            }
+
+        child1.getGene(0).addAllele(0, (int) parent2.getGene(0).getAllele(0));
+        int chosenCity = (int) parent2.getGene(0).getAllele(0);
+
+        for (int i = 1; i < child1.getLength(); i++) {
+            boolean conflict = false;
+            int j=0;
+            do {
+                int aux = chosenCity;
+                List<Integer> leastConnectedCities = this.getLeastConnectedCities(chosenCity, parent1.getLength(), distance);
+                chosenCity = leastConnectedCities.get(j);
+                //determine if there is conflict
+                conflict = false;
+                for (Gene g : child1.getGenes()) {
+                    if (g.getSize() != 0) {
+                        if (g.getAllele(0).equals(chosenCity)) {
+                            conflict = true;
+                            break;
                         }
                     }
-                    if(conflict){
-                        chosenCity = aux;
-                    }
-                    if(conflict && leastConnectedCities.size() == 1){
-                       majorConflict = true;
-                    }
-                }while(conflict && !majorConflict);
-                child1.getGene(i).addAllele(0, chosenCity);
-            }
-            j++;
-        }while(majorConflict);
-        
-        majorConflict = true;
-        j=0;
-        do{
-            majorConflict = false;
-            child2 = new Function(0.05, parent1.getLength(), false, c.getData());
-            child2.getGene(0).addAllele(0, (int)parent1.getGene(j).getAllele(0));
-            
-            int chosenCity=(int)parent1.getGene(j).getAllele(0);
-            for(int i=1; i<child2.getLength() && !majorConflict;i++){
-                boolean conflict=false;
-                do{
-                    int aux = chosenCity;
-                    List<Integer> leastConnectedCities = this.getLeastConnectedCities(chosenCity, parent1.getLength(), distance);
-                    chosenCity = this.getChosenCity(leastConnectedCities);
-                    //determine if there is conflict
-                    conflict = false;
-                    for(Gene g: child2.getGenes()){
-                        if(g.getSize() != 0){
-                            if(g.getAllele(0).equals(chosenCity)){
-                                conflict = true;
-                                break;
-                            }
+                }
+                if (conflict) {
+                    chosenCity = aux;
+                    j++;
+                }
+            } while (conflict);
+            child1.getGene(i).addAllele(0, chosenCity);
+        }
+
+        child2.getGene(0).addAllele(0, (int) parent1.getGene(0).getAllele(0));
+        chosenCity = (int) parent1.getGene(0).getAllele(0);
+        for (int i = 1; i < child2.getLength(); i++) {
+            boolean conflict = false;
+            int j=0;
+            do {
+                int aux = chosenCity;
+                List<Integer> leastConnectedCities = this.getLeastConnectedCities(chosenCity, parent1.getLength(), distance);
+                //determine if there is conflict
+                chosenCity = leastConnectedCities.get(j);
+                conflict = false;
+                for (Gene g : child2.getGenes()) {
+                    if (g.getSize() != 0) {
+                        if (g.getAllele(0).equals(chosenCity)) {
+                            conflict = true;
+                            break;
                         }
                     }
-                    if(conflict){
-                        chosenCity = aux;
-                    }
-                    if(conflict && leastConnectedCities.size() == 1){
-                       majorConflict = true;
-                    }
-                }while(conflict && !majorConflict);
-                child2.getGene(i).addAllele(0, chosenCity);
-            }
-        j++;
-        }while(majorConflict);
-        
-        
+                }
+                if (conflict) {
+                    chosenCity = aux;
+                    j++;
+                }
+            } while (conflict);
+            child2.getGene(i).addAllele(0, chosenCity);
+        }
+
         child1.evaluate();
         child2.evaluate();
         children.add(child1);
@@ -114,21 +97,21 @@ public class ERX extends Crossover {
 
         return children;
     }
-    
-    private List<Integer> getLeastConnectedCities(int initial, int length, int[][] distance){
-        HashMap<Integer,Integer> connectedCities = new HashMap<>();
-        
-        for(int i=0; i<length;i++){
-            if(distance[initial-1][i] != 0){
-                connectedCities.put(i+1, 0);
+
+    private List<Integer> getLeastConnectedCities(int initial, int length, int[][] distance) {
+        HashMap<Integer, Integer> connectedCities = new HashMap<>();
+
+        for (int i = 0; i < length; i++) {
+            if (distance[initial - 1][i] != 0) {
+                connectedCities.put(i + 1, 0);
             }
         }
-        
-        for(Map.Entry<Integer, Integer> entry : connectedCities.entrySet()) {
+
+        for (Map.Entry<Integer, Integer> entry : connectedCities.entrySet()) {
             Integer key = entry.getKey();
-            int numConnections=0;
-            for(int i=0; i<length;i++){
-                if(distance[key-1][i] != 0){
+            int numConnections = 0;
+            for (int i = 0; i < length; i++) {
+                if (distance[key - 1][i] != 0) {
                     numConnections++;
                 }
             }
@@ -136,35 +119,24 @@ public class ERX extends Crossover {
         }
 
         int minimumValue = Integer.MAX_VALUE;
-        
-        for(Map.Entry<Integer, Integer> entry : connectedCities.entrySet()) {
-            Integer value = entry.getValue();
-            if(value < minimumValue) {
-                minimumValue = value;
-            }
-        }
-        
+
         List<Integer> leastConnectedCities = new LinkedList<>();
-        
-        for(Map.Entry<Integer, Integer> entry : connectedCities.entrySet()) {
-            if(entry.getValue() == minimumValue) {
-              leastConnectedCities.add(entry.getKey());
-            }
-        }
-        
+
+        leastConnectedCities = connectedCities.entrySet().stream()
+                                .sorted(Comparator.comparing(Map.Entry::getValue))
+                                .map(Map.Entry::getKey)
+                                .collect(Collectors.toList());
+
         return leastConnectedCities;
     }
-    
-    private Integer getChosenCity(List<Integer> cities){
-        if(cities.size() == 1){
+
+    private Integer getChosenCity(List<Integer> cities) {
+        if (cities.size() == 1) {
             return cities.get(0);
-        }
-        else{
+        } else {
             Random randomizer = new Random();
             return cities.get(randomizer.nextInt(cities.size()));
         }
     }
 
 }
-
- 
